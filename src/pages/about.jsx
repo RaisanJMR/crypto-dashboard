32a4f36831461346af63ef935252200a0466da33
RaisanJMR <raisanjmr@protonmail.com>
2025-07-18 15:13:54 +0530

    add AboutPage component and Header for navigation

---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto modi recusandae autem repudiandae, dolore distinctio inventore itaque dolorem provident fugit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur officiis deleniti commodi tempora, accusamus accusantium quod eum dolor soluta eveniet?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, consectetur quibusdam quae eum quaerat id corporis</p>
+        </div>
+    )
+}
+
+export default AboutPage
