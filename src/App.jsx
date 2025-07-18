73c78002c0cb6fccfc042cc6689afdb382af7905
RaisanJMR <raisanjmr@protonmail.com>
2025-07-18 12:41:50 +0530

    add CoinCard and LimitSelector components; update App to use dynamic API URL and limit selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
-const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
+const API_URL = import.meta.env.VITE_API_URL
+
 const App = () => {
 
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [limit, setLimit] = useState(10)
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const res = await fetch(API_URL)
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`)
         if (!res.ok) throw new Error('Failed to fetch data')
         const data = await res.json()
         console.log(data)
@@ -23,28 +27,18 @@ const App = () => {
       }
     }
     fetchCoins()
-  }, [])
+  }, [limit])
 
   return (
     <div>
       <h1>crypto dash</h1>
       {loading && <p>loading..</p>}
       {error && <div className="error">{error}</div>}
+      <LimitSelector limit={limit} onLimitChange={setLimit} />
       {!loading && !error && (
         <main className="grid">
           {coins?.map((coin) => (
-            <div className="coin-card" key={coin.id}>
-              <div className="coin-header">
-                <img src={coin.image} alt={coin.name} className="coin-image" />
-              </div>
-              <h2>{coin.name}</h2>
-              <p className="symbol">{coin.symbol.toUpperCase()}</p>
-              <p> {new Intl.NumberFormat("en-GB", { style: "currency", currency: "USD" }).format(
-                coin.current_price,
-              )}</p>
-              <p className={coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}>{coin.price_change_percentage_24h.toFixed(2)} %</p>
-              <p>Market Cap: {coin.market_cap.toLocaleString()}</p>
-            </div>
+            <CoinCard coin={coin} key={coin.id} />
           ))}
         </main>
       )}
